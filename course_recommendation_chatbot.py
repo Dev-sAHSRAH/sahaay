fa69fa2244bc3e47f976d3c10376aabbd2b8c936
Harsha <97530701+Dev-sAHSRAH@users.noreply.github.com>
2024-11-26 13:35:26 +0530

    Update course_recommendation_chatbot.py

--- course_recommendation_chatbot.py
+++ course_recommendation_chatbot.py
@@ -20,7 +20,7 @@ def install_package(package):
 
 # List of required packages
 REQUIRED_PACKAGES = [
-    "streamlit", "langchain", "langchain_community", "langchain-huggingface", "openai", "tiktoken", "pandas"
+    "streamlit", "langchain", "langchain_community", "langchain-huggingface", "tiktoken", "pandas"
 ]
 
 # Prompt template
